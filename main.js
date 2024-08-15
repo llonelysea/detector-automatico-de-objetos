b393df65d7dd71191b3ed9c1a6993843a1e6eff4
kyo <114775706+llonelysea@users.noreply.github.com>
2024-08-15 15:41:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+img="";
+status = "";
+objects = [];
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console,log(results);
+    objects = results;
+}
+
+function preload(){
+    img= loadImage('dog.cat');
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status : Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i = 0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Objeto Detectado";
+            fill("#FF0000");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + " " = percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 290, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 190);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 150, 270, 240);
+}
